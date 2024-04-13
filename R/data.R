5b364f2f3abd9ca7542ca19be1d6965fa86a5efc
Fabio Fiorellato <flowerside@gmail.com>
2024-04-13 13:52:19 +0200

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' The reference gear groups colors
+#'
+#' @format
+#' \describe{
+#'   \item{GEAR_GROUP_CODE}{The gear group code}
+#'   \item{FILL}{The fill color for the identified gear group}
+#'   \item{COLOR}{The outline color for the identified gear group}
+#' }
+#' @export
+"REF_GEAR_GROUPS_COLORS"
