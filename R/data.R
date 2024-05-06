7b1c645d9d893451c8de02d11d95cc022a0934fe
Fabio Fiorellato <flowerside@gmail.com>
2024-05-06 15:24:11 +0200

    Fixes and updates

--- R/data.R
+++ R/data.R
@@ -1,5 +1,7 @@
 #' The reference gear groups colors
 #'
+#' Includes color definitions for the 'synthetic' OT gear group
+#'
 #' @format
 #' \describe{
 #'   \item{GEAR_GROUP_CODE}{The gear group code}
@@ -8,3 +10,14 @@
 #' }
 #' @export
 "REF_GEAR_GROUPS_COLORS"
+
+#' The reference catch types colors
+#'
+#' @format
+#' \describe{
+#'   \item{CATCH_TYPE_CODE}{The catch type code}
+#'   \item{FILL}{The fill color for the identified catch type}
+#'   \item{COLOR}{The outline color for the identified catch type}
+#' }
+#' @export
+"REF_CATCH_TYPES_COLORS"
--- data-raw/initialize_reference_data_aesthetics.R
+++ data-raw/initialize_reference_data_aesthetics.R
@@ -3,9 +3,12 @@
 library(iccat.pub.data)
 library(colorspace)
 library(RColorBrewer)
+library(Polychrome)
 
+### Gear groups
 REF_GEAR_GROUPS_COLORS = brewer.pal(n = 9, name = "Set1")
 REF_GEAR_GROUPS_COLORS = colorRampPalette(REF_GEAR_GROUPS_COLORS)(length(REF_GEAR_GROUPS$CODE))
+#REF_GEAR_GROUPS_COLORS = glasbey.colors(length(REF_GEAR_GROUPS$CODE))
 
 REF_GEAR_GROUPS_COLORS =
   data.table(
@@ -27,3 +30,26 @@ REF_GEAR_GROUPS_COLORS[, COLOR := darken(FILL, amount = .3)]
 REF_GEAR_GROUPS_COLORS = REF_GEAR_GROUPS_COLORS[order(GEAR_GROUP_CODE)]
 
 usethis::use_data(REF_GEAR_GROUPS_COLORS, overwrite = TRUE, compress = "gzip")
+
+### Catch types
+REF_CATCH_TYPES_COLORS = brewer.pal(n = 8, name = "Set2")
+REF_CATCH_TYPES_COLORS = colorRampPalette(REF_CATCH_TYPES_COLORS)(length(REF_CATCH_TYPES$CODE))
+
+REF_CATCH_TYPES_COLORS =
+  data.table(
+    CATCH_TYPE_CODE = REF_CATCH_TYPES$CODE,
+    FILL            = REF_CATCH_TYPES_COLORS[1:nrow(REF_CATCH_TYPES)]
+  )
+
+REF_CATCH_TYPES_COLORS$CATCH_TYPE_CODE =
+  factor(
+    REF_CATCH_TYPES_COLORS$CATCH_TYPE_CODE,
+    labels = REF_CATCH_TYPES$CODE,
+    levels = REF_CATCH_TYPES$CODE,
+    ordered = TRUE
+  )
+
+REF_CATCH_TYPES_COLORS[, COLOR := darken(FILL, amount = .3)]
+REF_CATCH_TYPES_COLORS = REF_CATCH_TYPES_COLORS[order(CATCH_TYPE_CODE)]
+
+usethis::use_data(REF_CATCH_TYPES_COLORS, overwrite = TRUE, compress = "gzip")
