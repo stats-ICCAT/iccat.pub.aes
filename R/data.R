cac79f5176779585fe0095a4531e91742887c268
Fabio Fiorellato <flowerside@gmail.com>
2024-08-12 13:12:24 +0200

    Adding species group colors

--- R/data.R
+++ R/data.R
@@ -11,6 +11,19 @@
 #' @export
 "REF_GEAR_GROUPS_COLORS"
 
+#' The reference species gear groups colors
+#'
+#' Includes color definitions for the 'synthetic' Others species gear group
+#'
+#' @format
+#' \describe{
+#'   \item{SPECIES_GEAR_GROUP}{The species gear group}
+#'   \item{FILL}{The fill color for the identified species gear group}
+#'   \item{COLOR}{The outline color for the identified species gear group}
+#' }
+#' @export
+"REF_SPECIES_GEAR_GROUPS_COLORS"
+
 #' The reference catch types colors
 #'
 #' @format
